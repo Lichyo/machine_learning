2455a7f24ad6a17a109078a5ca924af39b493bb6
Lichyo <lichyo003@gmail.com>
2024-03-22 01:51:16 +0800

    plt the trainging set result'

--- main.py
+++ main.py
@@ -2,37 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
-# import dataset
-dataset = pd.read_csv('Data.csv')
-features = dataset.iloc[:, :-1].values  # index locating ( row & column )
-dependent_variable = dataset.iloc[:, -1].values
+# load data
+data = pd.read_csv('Salary_Data.csv')
+X = data.iloc[:, :-1].values
+Y = data.iloc[:, -1].values
 
-# fill missing data with mean
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(features[:, 1:3])  # fit imputer with data
-features[:, 1:3] = imputer.transform(features[:, 1:3])  # replace missing data
+# fill missing values
+imputer = SimpleImputer(missing_values=np.NAN, strategy='mean')
+imputer.fit(X[:, :])
+X[:, :] = imputer.transform(X[:, :])
 
-# one hot encoding
-cd = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-features = np.array(cd.fit_transform(features))
+# no need to do one hot encoding
 
-# label encoding ->
-le = LabelEncoder()
-dependent_variable = np.array(le.fit_transform(dependent_variable))
+# split & feature scaling
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+# sc = StandardScaler()
+# x_train = sc.fit_transform(x_train[:, :])
+# x_test = sc.transform(x_test[:, :])
 
-# split training & test set
-x_train, x_test, y_train, y_test = train_test_split(features, dependent_variable, test_size=0.2)
-# print(len(x_train))
-# print(len(x_test))
 
-# feature scaling
-sc = StandardScaler()
-x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
-x_test[:, 3:] = sc.fit_transform(x_test[:, 3:])
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
 
+y_pre = regressor.predict(x_test)
+plt.scatter(x_train, y_train, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.title('Experience v.s. Salary')
+plt.xlabel('Years of Experience')
+plt.ylabel('Years of Salary')
+plt.show()
