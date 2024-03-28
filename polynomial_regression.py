bc4197a3fde8cbc89b3b8b279043cf998b91e0ac
Lichyo <lichyo003@gmail.com>
2024-03-28 15:46:01 +0800

    smooth curve

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
@@ -14,6 +15,17 @@ linear_reg = LinearRegression()
 linear_reg.fit(x_poly, Y)
 y_predict = linear_reg.predict(x_poly)
 
+# Polynomial Regression Plot
 plt.scatter(X, Y, color='blue')  # 散佈圖
 plt.plot(X, y_predict, color='red')  # Curve
+plt.show()
+
+# Smooth one
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color='red')
+plt.plot(X_grid, linear_reg.predict(poly_reg.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
 plt.show()
