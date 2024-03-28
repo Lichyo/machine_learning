5f3c85858b0225c599513a5dee669d93d006d414
Lichyo <lichyo003@gmail.com>
2024-03-28 15:49:31 +0800

    predict single value

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -16,16 +16,21 @@ linear_reg.fit(x_poly, Y)
 y_predict = linear_reg.predict(x_poly)
 
 # Polynomial Regression Plot
-plt.scatter(X, Y, color='blue')  # 散佈圖
-plt.plot(X, y_predict, color='red')  # Curve
-plt.show()
+# plt.scatter(X, Y, color='blue')  # 散佈圖
+# plt.plot(X, y_predict, color='red')  # Curve
+# plt.show()
+
+# Predict single value
+# test = poly_reg.fit_transform([[12]])
+# single_predict = linear_reg.predict(test)
+# print(single_predict)
 
 # Smooth one
-X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, Y, color='red')
-plt.plot(X_grid, linear_reg.predict(poly_reg.fit_transform(X_grid)), color='blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
+# X_grid = np.arange(min(X), max(X), 0.1)
+# X_grid = X_grid.reshape((len(X_grid), 1))
+# plt.scatter(X, Y, color='red')
+# plt.plot(X_grid, linear_reg.predict(poly_reg.fit_transform(X_grid)), color='blue')
+# plt.title('Truth or Bluff (Polynomial Regression)')
+# plt.xlabel('Position level')
+# plt.ylabel('Salary')
+# plt.show()
