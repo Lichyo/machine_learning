d909859d9b57a430d834c1ebed5c6cc132566e09
Lichyo <lichyo003@gmail.com>
2024-05-20 21:26:39 +0800

    analyzed with r2_score

--- single_feature_linear_regression.py
+++ single_feature_linear_regression.py
@@ -5,19 +5,17 @@ from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 # load data
 data = pd.read_csv('datasets/Salary_Data.csv')
 X = data.iloc[:, :-1].values
 Y = data.iloc[:, -1].values
 
-
 # fill missing values
 imputer = SimpleImputer(missing_values=np.NAN, strategy='mean')
 imputer.fit(X[:, :])
 X[:, :] = imputer.transform(X[:, :])
-
-# no need to do one hot encoding
 
 # split & feature scaling
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
@@ -27,6 +25,11 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(np.array(x_test).reshape(-1, 1))
+print(r2_score(y_test, y_pred))
+# print(r2_score(y_test, y_test))
+
 
 # y_pre = regressor.predict(x_test)
 # plt.scatter(x_test, y_test, color='red')
