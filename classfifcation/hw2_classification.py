64f9472f8d4677514848546337b5e56097c4d470
Lichyo <lichyo003@gmail.com>
2024-06-08 15:38:43 +0800

    submit homework

--- classfifcation/hw2_classification.py
+++ classfifcation/hw2_classification.py
@@ -15,6 +15,7 @@ from sklearn.impute import SimpleImputer
 import numpy as np
 import tools
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import recall_score, precision_score
 
 data = pd.read_csv('../datasets/HW2_heart.csv')
 raw = data
@@ -34,28 +35,36 @@ maxHR = tools.find_and_plot_outliers(raw, 'MaxHR')
 Oldpeak = tools.find_and_plot_outliers(raw, 'Oldpeak')
 raw = tools.delete_outliers(raw, 'Oldpeak', Oldpeak)
 
+# tools.plot_scatter(raw, 'Age', 'HeartDisease')
+# ['Age', 'RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak']
+# tools.heatmap(raw.drop(columns=['Sex', 'ChestPainType', 'FastingBS', 'RestingECG', 'ExerciseAngina', 'ST_Slope', 'HeartDisease']))
+
 # 敘述性統計分析 - CSV檔
-describe = raw.describe()
-describe.to_csv('out.csv')
-
-x = raw[:, :-1]
-y = raw[:, -1]
-# split data set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-
-# standard
-sc = StandardScaler()
-columns_to_scale = [3, 4, 7, 9]
-x_train[columns_to_scale] = sc.fit_transform(x_train[columns_to_scale])
-x_test[columns_to_scale] = sc.transform(x_test[columns_to_scale])
+# describe = raw.describe()
+# describe.to_csv('describe.csv')
 
 # Encoding Session
 ct = ColumnTransformer(
     transformers=[('encoder', OneHotEncoder(), ['Sex', 'ChestPainType', 'RestingECG', 'ExerciseAngina', 'ST_Slope'])],
     remainder='passthrough')
-x_train = np.array(ct.fit_transform(x_train))
-x_test = np.array(ct.fit_transform(x_test))
+data = ct.fit_transform(raw)
 
+x = data[:, :-1]
+y = data[:, -1]
+
+# split data set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+new_feature_names = ct.get_feature_names_out()
+numerical_features = ['RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak']
+columns_to_scale = [new_feature_names.tolist().index(f'remainder__{col}') for col in numerical_features]
+
+# standard
+sc = StandardScaler()
+x_train[:, columns_to_scale] = sc.fit_transform(x_train[:, columns_to_scale])
+x_test[:, columns_to_scale] = sc.transform(x_test[:, columns_to_scale])
+
+# print(data.describe())
 # building classifier
 knn_classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski')
 knn_classifier.fit(x_train, y_train)
@@ -82,8 +91,10 @@ def score(classifier, classifier_string):
     y_pred = classifier.predict(x_test)
     print(f'{classifier_string}')
     print(f'accuracy : {round(accuracy_score(y_pred, y_test), 3)}')
+    print(f'recall : {round(recall_score(y_pred, y_test), 3)}')
+    print(f'precision : {round(precision_score(y_pred, y_test), 3)}')
     print(f'f1_score : {round(f1_score(y_pred, y_test), 3)}')
-    print('confusion matrix')
+    print('confusion matrix : ')
     print(confusion_matrix(y_pred, y_test))
     print('\n')
 
@@ -91,8 +102,8 @@ def score(classifier, classifier_string):
 print('\n')
 score(knn_classifier, 'KNN_classifier')
 score(logistic_classifier, 'logistic_classifier')
-score(decision_tree_classifier, 'decision_tree_classifier')
+# score(decision_tree_classifier, 'decision_tree_classifier')
 score(random_forest_classifier, 'random_forest_classifier')
-score(linear_SVM, 'linear_SVM')
+# score(linear_SVM, 'linear_SVM')
 score(non_linear_SVM, 'non_linear_SVM')
-score(gaussianNB_classifier, 'gaussianNB_classifier')
+# score(gaussianNB_classifier, 'gaussianNB_classifier')
--- classfifcation/tools.py
+++ classfifcation/tools.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def find_and_plot_outliers(df, column_name):
@@ -10,7 +12,8 @@ def find_and_plot_outliers(df, column_name):
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
     outliers = data[(data < lower_bound) | (data > upper_bound)]
-    # print(outliers.to_string())
+
+    # print(outliers.to_string())  可以得到離群值的 list
     # plt.figure(figsize=(10, 6))
     # plt.boxplot(data, vert=False)
     # plt.scatter(outliers, np.ones(len(outliers)), color='red')
@@ -23,3 +26,32 @@ def find_and_plot_outliers(df, column_name):
 def delete_outliers(df, column_name, outliers):
     df_cleaned = df[~df[column_name].isin(outliers)]
     return df_cleaned
+
+
+def plot_scatter(data, x, y):
+    data[x].value_counts().plot.pie(autopct='%1.1f%%', figsize=(8, 8), cmap='Set3')
+    plt.title(f'Distribution of {x}')
+    plt.ylabel('')
+    plt.show()
+    # plt.scatter(data[x], data[y], color='red')
+    # plt.title(f'{x} v.s. {y}')
+    # plt.xlabel(f'{x}')
+    # plt.ylabel(f'{y}')
+    # plt.show()
+
+
+def heatmap(data):
+    corr = data.corr()
+    mask = np.ones(corr.shape).astype(bool)
+    mask = np.triu(mask, k=1)
+    ax = sns.heatmap(
+        corr, vmin=-1, vmax=1, center=0,
+        cmap=sns.diverging_palette(20, 220, n=200),
+        square=True, annot=True
+    )
+    ax.set_xticklabels(
+        ax.get_xticklabels(),
+        rotation=45,
+        horizontalalignment='right'
+    )
+    plt.show()
